d87575ab61e46f53201ffdc23daf6d36e6f31c65
Tyler Barham <tyler.r.barham@gmail.com>
2024-08-02 11:17:20 +1200

    Add layer taps, mv shift+symbols to sym layer, and mv media keys

--- keyboards/splitkb/kyria/rev2/keymaps/Tyler-Barham/keymap.c
+++ keyboards/splitkb/kyria/rev2/keymaps/Tyler-Barham/keymap.c
@@ -31,34 +31,33 @@ enum layers {
 #define NAV      MO(_NAV)
 #define FKEYS    MO(_FUNCTION)
 
-#define CTL_ESC  MT(MOD_LCTL, KC_ESC)
+#define NAV_ENT  LT(_NAV, KC_ENT)
+#define SYM_TAB  LT(_SYM, KC_TAB)
+
 #define CTL_TAB  MT(MOD_LCTL, KC_TAB)
 #define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
-#define CTL_MINS MT(MOD_RCTL, KC_MINUS)
+#define CTL_DEL  MT(MOD_LCTL, KC_DEL)
 #define ALT_ENT  MT(MOD_LALT, KC_ENT)
-
-// Note: LAlt/Enter (ALT_ENT) is not the same thing as the keyboard shortcut Alt+Enter.
-// The notation `mod/tap` denotes a key that activates the modifier `mod` when held down, and
-// produces the key `tap` when tapped (i.e. pressed and released).
+#define ALT_BSPC MT(MOD_LALT, KC_BSPC)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
+       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_COLN,
       CTL_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,CTL_QUOT,
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC, KC_CAPS,           FKEYS, KC_RBRC,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-                                 _______, _______, KC_LGUI,  KC_SPC,     NAV,             SYM, ALT_ENT, KC_RGUI, _______, _______
+                                 _______, _______, KC_LGUI,  KC_SPC, NAV_ENT,         SYM_TAB,ALT_BSPC, CTL_DEL, _______, _______
     ),
     [_NAV] = LAYOUT(
-      _______, _______, _______, _______, _______, _______,                                            KC_PGUP, KC_HOME,KC_PAUSE,  KC_END, KC_VOLU,  KC_DEL,
-      _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                                            KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_VOLD,  KC_INS,
-      _______, _______, _______, _______, _______, _______, _______, KC_SCRL,        _______, _______, KC_PGDN, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
+      _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU,                                            KC_PGUP, KC_HOME,  KC_END, _______, _______,  KC_DEL,
+      _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_VOLD,                                            KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______,  KC_INS,
+      _______, _______, _______, _______, _______, KC_MUTE, _______, KC_SCRL,        _______, _______, KC_PGDN, _______, _______, _______, _______, KC_PSCR,
                                  _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______
     ),
     [_SYM] = LAYOUT(
        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_EQL,
       KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
-      KC_PIPE, KC_BSLS, KC_COLN, KC_SCLN, KC_MINS, KC_LBRC, KC_LCBR, _______,        _______, KC_RCBR, KC_RBRC, KC_UNDS, KC_COMM,  KC_DOT, KC_SLSH, KC_QUES,
+      KC_PIPE, KC_BSLS, KC_COLN, KC_SCLN, KC_MINS, KC_LBRC, KC_LCBR, _______,        _______, KC_RCBR, KC_RBRC, KC_UNDS,   KC_LT,   KC_GT, KC_QUES, _______,
                                  _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______
     ),
     [_FUNCTION] = LAYOUT(
