004613cd663396ba5562984894c2cccdbb0d4af8
Tyler Barham <tyler.r.barham@gmail.com>
2024-08-14 12:16:59 +1200

    remove ctrl from kc_quote to fix rollover

--- keyboards/splitkb/kyria/rev2/keymaps/Tyler-Barham/keymap.c
+++ keyboards/splitkb/kyria/rev2/keymaps/Tyler-Barham/keymap.c
@@ -35,16 +35,14 @@ enum layers {
 #define SYM_TAB  LT(_SYM, KC_TAB)
 
 #define CTL_TAB  MT(MOD_LCTL, KC_TAB)
-#define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
 #define CTL_DEL  MT(MOD_LCTL, KC_DEL)
-#define ALT_ENT  MT(MOD_LALT, KC_ENT)
 #define ALT_BSPC MT(MOD_LALT, KC_BSPC)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_COLN,
-      CTL_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,CTL_QUOT,
+      CTL_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,KC_QUOTE,
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC, KC_CAPS,           FKEYS, KC_RBRC,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
                                  _______, _______, KC_LGUI,  KC_SPC, NAV_ENT,         SYM_TAB,ALT_BSPC, CTL_DEL, _______, _______
     ),
